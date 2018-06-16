feb43f2e6be6b5a758e3d3e65bba18d1ad9c2fb0
Nathaniel Qin <nathaniel.qin1@gmail.com>
2018-06-16 15:15:09 -0700

    made puzzle 2 easier

--- myscripts.js
+++ myscripts.js
@@ -1,83 +1,58 @@
+function toSuccess() {
+  alert("uh.....")
+  window.location.href = "../success.html"
+}
+
+function alertWrong() {
+  var seed = Math.floor(Math.random()*10);
+  switch(seed) {
+    case 0:
+      alert("Try a little harder (:")
+      break;
+    case 1:
+      alert("Try again ):")
+      break;
+    case 2:
+      alert("Not even close... o.O")
+      break;
+    case 3:
+      alert("Excuse me? -__-")
+      break;
+    case 4:
+      alert("Not quite c:")
+      break;
+    case 5:
+      alert("Nope... =.=")
+      break;
+    case 6:
+      alert("Huh? |:")
+      break;
+    case 7:
+      alert("Guess again. *_*")
+      break;
+    case 8:
+      alert("Was this too hard? ._.")
+      break;
+    case 9:
+      alert("Keep guessing, I believe in you xD")
+      break;
+    default:
+      alert("If you see this message you have hacked the system. +_+")
+  }
+}
+
 function checkAnswer1(guess) {
   if(guess === "albuquerque") {
-    window.location.href = "../success.html";
+    toSuccess();
   } else {
-    var seed = Math.floor(Math.random()*10);
-    switch(seed) {
-      case 0:
-        alert("Try a little harder (:")
-        break;
-      case 1:
-        alert("Try again ):")
-        break;
-      case 2:
-        alert("Not even close... o.O")
-        break;
-      case 3:
-        alert("Excuse me? -__-")
-        break;
-      case 4:
-        alert("Not quite c:")
-        break;
-      case 5:
-        alert("Nope... =.=")
-        break;
-      case 6:
-        alert("Huh? |:")
-        break;
-      case 7:
-        alert("Guess again. *_*")
-        break;
-      case 8:
-        alert("Was this too hard? ._.")
-        break;
-      case 9:
-        alert("Keep guessing, I believe in you xD")
-        break;
-      default:
-        alert("If you see this message you have hacked the system. +_+")
-    }
+    alertWrong();
   }
 }
 
 function checkAnswer2(guess) {
-  if(guess === "pineapples belong on pizza" || guess === "pineapplesbelongonpizza") {
-    window.location.href = "../success.html";
+  if(guess === "pineapple belongs on pizza" || guess === "pineapplebelongsonpizza") {
+    toSuccess();
   } else {
-    var seed = Math.floor(Math.random()*10);
-    switch(seed) {
-      case 0:
-        alert("Try a little harder (:")
-        break;
-      case 1:
-        alert("Try again ):")
-        break;
-      case 2:
-        alert("Not even close... o.O")
-        break;
-      case 3:
-        alert("Excuse me? -__-")
-        break;
-      case 4:
-        alert("Not quite c:")
-        break;
-      case 5:
-        alert("Nope... =.=")
-        break;
-      case 6:
-        alert("Huh? |:")
-        break;
-      case 7:
-        alert("Guess again. *_*")
-        break;
-      case 8:
-        alert("Was this too hard? ._.")
-        break;
-      case 9:
-        alert("Keep guessing, I believe in you xD")
-        break;
-      default:
-        alert("If you see this message you have hacked the system. +_+")
-    }
+    alertWrong();
   }
 }
