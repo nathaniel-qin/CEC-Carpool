27271e0096415f345419a253146fdad6a9d198cf
Nathaniel Qin <nathaniel.qin1@gmail.com>
2018-06-15 23:18:26 -0700

    added success page, updated scripts

---
+++ myscripts.js
@@ -0,0 +1,41 @@
+function checkAnswer(guess) {
+  if(guess === "albuquerque") {
+    window.location.href = "../success.html";
+  } else {
+    var seed = Math.floor(Math.random()*10);
+    switch(seed) {
+      case 0:
+        alert("Try a little harder (:")
+        break;
+      case 1:
+        alert("Try again ):")
+        break;
+      case 2:
+        alert("Not even close... o.O")
+        break;
+      case 3:
+        alert("Excuse me? -__-")
+        break;
+      case 4:
+        alert("Not quite c:")
+        break;
+      case 5:
+        alert("Nope... =.=")
+        break;
+      case 6:
+        alert("Huh? |:")
+        break;
+      case 7:
+        alert("Guess again. *_*")
+        break;
+      case 8:
+        alert("Was this too hard? ._.")
+        break;
+      case 9:
+        alert("Keep guessing, I believe in you xD")
+        break;
+      default:
+        alert("If you see this message you have hacked the system. +_+")
+    }
+  }
+}
